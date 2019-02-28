6cd91a4c3cf6b47f63b442c1ab6a04b5e81c673f
Paul <paul_lin@foxmail.com>
2019-02-28 19:45:44 +0800

    add

--- include/parser.h
+++ include/parser.h
@@ -45,4 +45,29 @@ static ExprAst *ParseNumberExpr() {
     return Result;
 }
 
+// 解析表达式
+static ExprAst *ParseExpression() {
+    return nullptr;
+}
+
+// 解析括号运算符
+// 该函数的特点:
+// 1. 它展示了 Error 函数的用法，将错误信息输出到控制台，同时返回一个nullptr
+// 2. 递归的调用了 ParseExpression (很快我们就会看到ParseExpression还会反过来调用ParseParenExpr)
+// 在解析的过程中，丢弃了括号，因为当语法解析器构造完AST之后，括号就没用了.
+static ExprAst *ParseParenExpr() {
+    getNextToken(); // 消耗 (
+    ExprAst *V = ParseExpression();
+
+    if (!V) {
+        return nullptr;
+    }
+
+    if (CurTok != ')') {
+        return Error("expected ')'");
+    }
+    getNextToken();
+    return V;
+}
+
 #endif //TINA_LANG_PARSER_H
