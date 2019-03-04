2ac93ec41e7effc6eea24c97519b8508aec7f25b
Paul <paul_lin@foxmail.com>
2019-03-04 19:38:18 +0800

    增加语法树

--- include/ast.h
+++ include/ast.h
@@ -53,24 +53,37 @@ class VariableExprAst : public ExprAst {
 public:
     explicit VariableExprAst(const std::string &name) : Name(name) {}
 
+    llvm::Value *Codegen();
 };
 
+llvm::Value *VariableExprAst::Codegen() {
+    return nullptr;
+}
 // 运算符表达式
 class BinaryExprAst : public ExprAst {
     char Op;
     ExprAst *LHS, *RHS;
 public:
     BinaryExprAst(char op, ExprAst *lhs, ExprAst *rhs) : Op(op), LHS(lhs), RHS(rhs) {}
+
+    llvm::Value *Codegen();
 };
 
+llvm::Value *BinaryExprAst::Codegen() {
+    return nullptr;
+}
 // 函数名和用作参数的表达式
 class CallExprAst : public ExprAst {
     std::string Callee;
     std::vector<ExprAst *> Args;
 public:
     CallExprAst(const std::string &callee, std::vector<ExprAst *> &args) : Callee(callee), Args(args) {}
-};
 
+    llvm::Value *Codegen();
+};
+llvm::Value *CallExprAst::Codegen() {
+    return nullptr;
+}
 // 函数接口
 class PrototypeAst {
     std::string Name;
