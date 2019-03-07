487692cddad4c67775280ec0d536b059c8b0c9c3
Paul <paul_lin@foxmail.com>
2019-03-07 19:35:33 +0800

    取消jit的文件

--- include/jit.h
+++ include/jit.h
@@ -4,33 +4,32 @@
 
 #ifndef TINA_LANG_JIT_H
 #define TINA_LANG_JIT_H
-#include "llvm/ADT/StringRef.h"
-#include "llvm/ExecutionEngine/JITSymbol.h"
-#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
-#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
-#include "llvm/ExecutionEngine/Orc/Core.h"
-#include "llvm-c/Core.h"
-#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
-#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
-#include "llvm/ExecutionEngine/SectionMemoryManager.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/LLVMContext.h"
-#include <memory>
-
-namespace tina{
-    namespace orc{
-        class Jit{
-        private:
-            llvm::orc::ExecutionSession ES;
-            llvm::orc::RTDyldObjectLinkingLayer ObjectLayer;
-            llvm::orc::IRCompileLayer CompileLayer;
-            llvm::DataLayout DL;
-            llvm::orc::MangleAndInterner Mangle;
-            llvm::orc::ThreadSafeContext Ctx;
-        public:
-            Jit(llvm::orc::JITTargetMachineBuilder JTMB,llvm::orc::DataLayout DL):ObjectLayer(ES,[](){return llvm::make_unique<llvm::orc::SectionMemoryManager>();}),
-                                                                        CompileLayer(ES){}
-        };
-    }
-}
+//#include "llvm/ADT/StringRef.h"
+//#include "llvm/ExecutionEngine/JITSymbol.h"
+//#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+//#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
+//#include "llvm/ExecutionEngine/Orc/Core.h"
+//#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+//#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+//#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+//#include "llvm/IR/DataLayout.h"
+//#include "llvm/IR/LLVMContext.h"
+//#include <memory>
+//
+//namespace tina{
+//    namespace orc{
+//        class Jit{
+//        private:
+//            llvm::orc::ExecutionSession ES;
+//            llvm::orc::RTDyldObjectLinkingLayer ObjectLayer;
+//            llvm::orc::IRCompileLayer CompileLayer;
+//            llvm::DataLayout DL;
+//            llvm::orc::MangleAndInterner Mangle;
+//            llvm::orc::ThreadSafeContext Ctx;
+//        public:
+//            Jit(llvm::orc::JITTargetMachineBuilder JTMB,llvm::orc::DataLayout DL):ObjectLayer(ES,[](){return llvm::make_unique<llvm::orc::SectionMemoryManager>();}),
+//                                                                        CompileLayer(ES){}
+//        };
+//    }
+//}
 #endif //TINA_LANG_JIT_H
